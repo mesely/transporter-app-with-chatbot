1f0e93f1d401cdc2290da0bd92cb741668f82d0e
selman <selman.yilmaz@sabanciuniv.edu>
2026-02-10 02:22:22 +0300

    sf

--- kamyon/src/data/data.module.ts
+++ kamyon/src/data/data.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DataService } from './data.service';
 
-import { UsersModule } from '../users/users.module';
-import { MigrationController } from './data.controller';
+// 1. Şemaları İmport Et
+import { Profile, ProfileSchema } from '../users/schemas/profile.schema'; // Yolu kendi dosya yapına göre düzelt
+import { NewUser, NewUserSchema } from './schemas/new-user.schema';
+import { NewProvider, NewProviderSchema } from './schemas/new-provider.schema';
+import { MigrationController } from './migration.controller';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [MigrationController],
-  providers: [DataService],
-  exports: [DataService],
+  imports: [
+    // 2. MongooseModule.forFeature ile bu modüle modelleri tanıt
+    MongooseModule.forFeature([
+      { name: Profile.name, schema: ProfileSchema },      // Eski Tablo
+      { name: NewUser.name, schema: NewUserSchema },      // Yeni Kullanıcı Tablosu
+      { name: NewProvider.name, schema: NewProviderSchema } // Yeni Hizmet Tablosu
+    ]),
+  ],
+  controllers: [MigrationController], // Endpoint için controller
+  providers: [DataService],           // Servisimiz
+  exports: [DataService]              // Başka yerde kullanacaksan dışa aç
 })
 export class DataModule {}
